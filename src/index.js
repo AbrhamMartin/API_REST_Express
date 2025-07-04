b06ec2ec6cf8c3fab7f68394abdca1fc36a171b2
abrhammartin <canchemartin2410@gmail.com>
2025-07-03 18:24:31 -0600

    implement cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express"
+import cors from "cors"
 import products from "./Routes/products.route.js"
-import { PORT } from "./Database/config.js"
+import { PORT, HOST_CLIENT } from "./Database/config.js"
 
 const app = express()
 app.use(express.json());
+app.use(cors({origin:HOST_CLIENT}))
 app.use('/api',products)
 app.listen(PORT)
